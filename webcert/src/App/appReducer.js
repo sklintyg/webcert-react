a7b3604f8c64347ccfa49a148baef23e735e3e11
Mattias Pettersson <mattias.pettersson@soprasteria.com>
2020-03-03 07:49:39 +0100

    Create appReducer
    reducer to manage the global app state

---
+++ webcert/src/App/appReducer.js
@@ -0,0 +1,11 @@
+const inititalState = {};
+
+const appStateReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
+
+export { inititalState };
+export default appStateReducer;
