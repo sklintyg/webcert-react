9e03d5d9f68a5cc26caeda2e21ffedca1bba7d8b
Mattias Pettersson <mattias.pettersson@soprasteria.com>
2020-03-03 09:47:19 +0100

    Update App with Header and Navigation component

--- webcert/src/App/App.js
+++ webcert/src/App/App.js
@@ -1,32 +1,23 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import { AppStateProvider, useAppState } from "./app-state";
 import appReducer, { inititalState } from "./appReducer";
 
+import { Header } from "../components/Header";
+import { Navigation } from "../components/Navigation";
+
 function App() {
-  const [appState, appDispatch] = useAppState();
+  const [, appDispatch] = useAppState();
 
   useEffect(() => {
-    appDispatch({ type: "DEMO_NAME", name: "userName" });
-  }, []);
+    Promise.resolve(appDispatch({ type: "DEMO_NAME", name: "userName" }));
+  }, [appDispatch]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navigation />
     </div>
   );
 }
