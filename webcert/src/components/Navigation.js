a8cbb29925a34a5988f8be0e95c8b88c5351dbac
Mattias Pettersson <mattias.pettersson@soprasteria.com>
2020-03-04 13:38:19 +0100

    Update navigation with proper text and background color

--- webcert/src/components/Navigation.js
+++ webcert/src/components/Navigation.js
@@ -1,23 +1,28 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useReducer, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-const About = lazy(() =>
-  import("../components/About" /*webpackChunkName: about */)
-);
+
+import styles from "./Navigation.module.scss";
+const About = lazy(() => import("../components/About"));
+const ChoosePatient = lazy(() => import("./ChoosePatient"));
+const Intyg = lazy(() => import("./Intyg"));
 
 export function Navigation() {
   return (
     <Router>
       <div>
-        <nav>
+        <nav className={styles.navigation}>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/create">Sök / skriv intyg</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/enhet-arenden">Ej hanterade ärenden</Link>
+            </li>
+            <li>
+              <Link to="/unsigned">Ej signerade utkast</Link>
             </li>
           </ul>
         </nav>
@@ -25,6 +30,16 @@ export function Navigation() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
+          <Route exact path="/create">
+            <Suspense fallback={<p>Laddar data...</p>}>
+              <ChoosePatient />
+            </Suspense>
+          </Route>
+          <Route path="/create/choose-intyg-type/:id">
+            <Suspense fallback={<p>Laddar data...</p>}>
+              <Intyg />
+            </Suspense>
+          </Route>
           <Route path="/about">
             <Suspense fallback={<p>Laddar data...</p>}>
               <About />
