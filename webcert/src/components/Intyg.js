2aef2f212efa02490575afe9bf21db9adfa71d6b
Mattias Pettersson <mattias.pettersson@soprasteria.com>
2020-03-06 13:07:57 +0100

    Update Intyg to create new intyg

--- webcert/src/components/Intyg.js
+++ webcert/src/components/Intyg.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useReducer } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import styles from "./Intyg.module.scss";
 import { WebcertMock } from "../Services/WebcertService";
@@ -8,6 +8,8 @@ import { ErrorBoundary } from "./ErrorBoundary";
 
 function reducer(state, action) {
   switch (action.type) {
+    case "CREATE_INTYG":
+      return { ...state, status: action.status, newIntyg: action.newIntyg };
     case "LOAD_PATIENT":
       console.log("LOAD_PATIENT", action);
       return { ...state, person: action.person };
@@ -20,12 +22,15 @@ function reducer(state, action) {
 export default function Intyg() {
   const [state, dispatch] = useReducer(reducer, {
     person: {},
-    intyg: undefined
+    intyg: undefined,
+    status: "idle",
+    newIntyg: undefined
   });
 
   const webcertMock = new WebcertMock();
-  const { person, intyg } = state;
+  const { person, intyg, status, newIntyg } = state;
   let { id } = useParams();
+  let history = useHistory();
 
   useEffect(() => {
     let isCurrent = true;
@@ -57,6 +62,24 @@ export default function Intyg() {
     return () => (isCurrent = false);
   }, [id, webcertMock, intyg]);
 
+  useEffect(() => {
+    let isCurrent = true;
+    const isIdle = status === "idle";
+    const isLoading = status === "loading";
+    const isSuccess = status === "success";
+    const isError = status === "error";
+
+    if (isLoading && newIntyg) {
+      console.log("SKAPA INTYG kod:", newIntyg);
+      webcertMock.createIntyg().then(data => {
+        console.log("SKAPAT INTYG:", data);
+        history.push(`/${newIntyg}/edit/${data.metadata.intygId}`);
+      });
+    }
+
+    return () => (isCurrent = false);
+  }, [status, newIntyg, webcertMock]);
+
   console.log("person", person);
   return (
     <>
@@ -75,7 +98,18 @@ export default function Intyg() {
               <li key={index}>
                 {item.name} {item.code}{" "}
                 <span className="text-right">
-                  <Button className={styles.createIntyg}>Skapa intyg</Button>
+                  <Button
+                    className={styles.createIntyg}
+                    onClick={() =>
+                      dispatch({
+                        type: "CREATE_INTYG",
+                        status: "loading",
+                        newIntyg: item.code
+                      })
+                    }
+                  >
+                    Skapa intyg
+                  </Button>
                 </span>
               </li>
             ))}
