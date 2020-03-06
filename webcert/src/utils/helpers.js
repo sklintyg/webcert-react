b267558eeffc6b6d52a9bca5b18ab79b52ae7add
Mattias Pettersson <mattias.pettersson@soprasteria.com>
2020-03-06 13:09:24 +0100

    Add generateUUID to helpers

--- webcert/src/utils/helpers.js
+++ webcert/src/utils/helpers.js
@@ -10,3 +10,13 @@ export function formatPersonnr(personnr) {
 
   return response;
 }
+
+export function generateUUID() {
+  var d = new Date().getTime();
+  var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === "x" ? r : (r & 0x7) | 0x8).toString(16);
+  });
+  return uuid;
+}
