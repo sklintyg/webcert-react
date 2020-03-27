f9f37ce78f60171102a36f74865228c3dda118c5
Mattias Pettersson <mattias.pettersson@soprasteria.com>
2020-03-27 12:56:09 +0100

    Create DynamicForm
    Use to buid certificates from data provided
    by the api

---
+++ webcert/src/components/Certificates/DynamicForm.js
@@ -0,0 +1,157 @@
+import React, { useReducer, useEffect, lazy } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+import { useParams } from "react-router-dom";
+
+import styles from "./Certificates.module.scss";
+import { WebcertMock } from "../../Services/WebcertService";
+
+import { Check } from "../shared/Check";
+import { Radio } from "../shared/Radio";
+import { Textfield } from "../shared/Textfield";
+
+const YesNoComment = lazy(() => import("./SubQuestion/YesNoComment"));
+const AF00213 = lazy(() => import("./AF00213"));
+const FK7804 = lazy(() => import("./FK7804"));
+
+function renderCert(certificateCode) {
+  switch (certificateCode) {
+    case "AF00213":
+      return (
+        <React.Suspense fallback={<p>Laddar data...</p>}>
+          <AF00213 />
+        </React.Suspense>
+      );
+    case "FK7804":
+      return (
+        <React.Suspense fallback={<p>Laddar data...</p>}>
+          <FK7804 />
+        </React.Suspense>
+      );
+    default:
+      return null;
+  }
+}
+
+function renderQuestions(item, data, dispatch, index) {
+  switch (item.type) {
+    case "YES_NO_COMMENT":
+      return (
+        <YesNoComment
+          key={index}
+          index={index}
+          data={data}
+          update={newValue =>
+            dispatch({
+              type: "UPDATE",
+              name: newValue.name,
+              value: newValue.value
+            })
+          }
+          {...item}
+        />
+      );
+    default:
+      return;
+  }
+}
+
+function reducer(state, action) {
+  let { data } = state;
+
+  switch (action.type) {
+    case "LOAD_CERTIFICATE":
+      return { ...state, certElement: action.certElement };
+    case "LOAD_DATA":
+      return { ...state, data: action.data, metadata: action.metadata };
+    case "LOAD_ELEMENTS":
+      return { ...state, elements: action.elements };
+
+    case "SUBMIT":
+      return { ...state, save: !state.save, status: action.status };
+    case "UPDATE":
+      data[action.name] = action.value;
+      return { ...state, data };
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
+export default function DynamicForm() {
+  let { certificateCode, certificateId } = useParams();
+  certificateCode = "AF00213";
+
+  const [state, dispatch] = useReducer(reducer, {
+    data: {},
+    metadata: {},
+    elements: null,
+    certElement: null,
+    save: false,
+    status: "idle"
+  });
+  const { data, elements, save, status, metadata } = state;
+  const webcertService = new WebcertMock();
+
+  useEffect(() => {
+    let isCurrent = true;
+
+    function getCertificate() {
+      if (certificateCode && !elements) {
+        console.log();
+        webcertService
+          .createDynamicCertificate(certificateCode)
+          .then(response => {
+            if (isCurrent) {
+              dispatch({
+                type: "LOAD_DATA",
+                data: response.data,
+                metadata: response.metadata
+              });
+              const sections = response.sections;
+
+              const createElements = sections.map((section, index) =>
+                renderQuestions(section, response.data, dispatch, index)
+              );
+              dispatch({
+                type: "LOAD_ELEMENTS",
+                elements: createElements
+              });
+            }
+          });
+      }
+    }
+
+    if (!certificateId) {
+      getCertificate();
+    } else {
+      console.log("TODO certId", certificateId);
+    }
+    return () => (isCurrent = false);
+  }, [elements, webcertService, certificateCode, data, certificateId]);
+
+  useEffect(() => {
+    function submitCertificate() {
+      webcertService
+        .saveData(metadata.certificateId, data, metadata)
+        .then(() => {
+          dispatch({ type: "SUBMIT", status: "idle" });
+        });
+    }
+
+    if (save) {
+      submitCertificate();
+    }
+  }, [save, data, metadata, webcertService]);
+
+  return (
+    <div>
+      {elements}
+      <Button
+        disable={status === "loading"}
+        onClick={() => dispatch({ type: "SUBMIT", status: "loading" })}
+      >
+        Signera och skicka
+      </Button>
+    </div>
+  );
+}
