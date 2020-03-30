87a60d8624d469135eb3668cd4ab2902212923ab
Mattias Pettersson <mattias.pettersson@soprasteria.com>
2020-03-30 08:36:48 +0200

    Add more data to webcerservice mock

--- webcert/src/Services/WebcertService.js
+++ webcert/src/Services/WebcertService.js
@@ -14,7 +14,7 @@ export class WebcertMock {
     return promise;
   }
 
-  getCertificate() {
+  getCertificates() {
     const data = [
       {
         name: "Arbetsförmedlingens medicinska utlåtande",
@@ -45,27 +45,132 @@ export class WebcertMock {
     return promise;
   }
 
+  getCurrentCertificates() {
+    const webcertData = localStorage.getItem("webcert");
+    const promise = new Promise((resolve, reject) => {
+      const webcert = webcertData ? JSON.parse(webcertData) : { data: [] };
+      resolve(webcert);
+    });
+
+    return promise;
+  }
   createCertificate(certificateCode) {
     const certificateId = generateUUID();
 
     const promise = new Promise(resolve => {
-      const data = {
+      const data = _getData(certificateCode, certificateId);
+
+      resolve(data);
+    });
+
+    return promise;
+  }
+
+  createDynamicCertificate(certificateCode) {
+    const certificateId = generateUUID();
+
+    const promise = new Promise(resolve => {
+      const data = _getData(certificateCode, certificateId);
+
+      resolve(data);
+    });
+
+    return promise;
+  }
+
+  saveData(id, data, metadata) {
+    debugger;
+    const webcertData = localStorage.getItem("webcert");
+    const webcert = webcertData ? JSON.parse(webcertData) : { data: [] };
+    const hasData = webcert.data.filter(item => item.id === id);
+
+    if (hasData.length > 0) {
+    } else {
+      webcert.data.push({ id, data, metadata });
+    }
+
+    const promise = new Promise((resolve, reject) => {
+      localStorage.setItem("webcert", JSON.stringify(webcert));
+      resolve();
+    });
+
+    return promise;
+  }
+}
+
+function _getData(certificateCode, certificateId) {
+  let data = {
+    metadata: {
+      certificateCode,
+      certificateId
+    }
+  };
+  switch (certificateCode) {
+    case "AF00213":
+      data = {
         metadata: {
           certificateCode,
-          certificateId
+          certificateId,
+          certificateName: "Arbetsförmedlingens medicinska utlåtande"
         },
-        fields: [
+        sections: [
           {
-            title: "Funktionsnedsättning",
-            name: "funktionsnedsattning",
-            type: "CHECKBOX",
+            type: "YES_NO_COMMENT",
             header: "Funktionsnedsättning",
-            label: "Ja",
+            required: true,
             text:
               "Finns besvär på grund av sjukdom eller skada som medför funktionsnedsättning?",
-            defaultValue: false
+            questionProp: "funktionsnedsattning",
+            descriptionProp: "funktionsnedsattningDesc",
+            descriptionLabel:
+              "Beskriv de funktionsnedsättningar som har observerats (undersökningsfynd). Ange, om möjligt, varaktighet. "
+          },
+          {
+            type: "YES_NO_COMMENT",
+            required: true,
+            header: "Aktivitetsbegränsning",
+            text:
+              "Leder funktionsnedsättningarna till aktivitetsbegränsningar i relation till arbete eller studier?",
+            questionProp: "aktivitetsbegransningar",
+            descriptionLabel:
+              "Ange vilka aktivitetsbegränsningar? Ange hur och om möjligt varaktighet/prognos.",
+            descriptionProp: "aktivitetsbegransningarDesc"
+          },
+          {
+            type: "YES_NO_COMMENT",
+            required: true,
+            header: "Utredning och behandling",
+            text:
+              "Finns pågående eller planerade utredningar/behandlingar som påverkar den planering som Arbetsförmedlingen har beskrivit i förfrågan?",
+            questionProp: "utredningBehandling",
+            descriptionProp: "utredningBehandlingDesc",
+            descriptionLabel:
+              "Hur påverkar utredningarna/behandlingarna planeringen? När planeras utredningarna/behandlingarna att vara avslutade?"
+          },
+          {
+            type: "YES_NO_COMMENT",
+            header: "Arbetets påverkan på sjukdom/skada",
+            text:
+              "Kan sjukdomen/skadan förvärras av vissa arbetsuppgifter/arbetsmoment?",
+            questionProp: "arbetePaverkanSjukdomSkada",
+            descriptionProp: "arbetePaverkanSjukdomSkadaDesc",
+            descriptionLabel: "Vilken typ av arbetsuppgifter/arbetsmoment?"
           }
         ],
+        data: {
+          funktionsnedsattning: false,
+          funktionsnedsattningDesc: "",
+          aktivitetsbegransningar: false,
+          aktivitetsbegransningarDesc: "",
+          utredningBehandling: false,
+          utredningBehandlingDesc: "",
+          arbetePaverkanSjukdomSkada: false,
+          arbetePaverkanSjukdomSkadaDesc: ""
+        }
+      };
+      return data;
+    case "FK7804":
+      data = {
         data: {
           funktionsnedsattning: false,
           funktionsnedsattningDesc: "",
@@ -75,10 +180,9 @@ export class WebcertMock {
           arbetePaverkanSjukdomSkadaDesc: ""
         }
       };
-
-      resolve(data);
-    });
-
-    return promise;
+      debugger;
+      return data;
+    default:
+      return data;
   }
 }
