e46bdc96280c38cd1b7b9ee3996f1174474f3905
Mattias Pettersson <mattias.pettersson@soprasteria.com>
2020-04-16 20:51:24 +0200

    Create Af00213 certificate

--- webcert/src/components/Certificates/Af00213.js
+++ webcert/src/components/Certificates/Af00213.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { lazy, Suspense, useReducer, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
@@ -7,22 +7,240 @@ import FormControl from "react-bootstrap/FormControl";
 
 import styles from "./Certificates.module.scss";
 
+import { WebcertMock } from "../../Services/WebcertService";
+
+import { UeRadio } from "../shared/UeRadio";
+
+const UeDetails = lazy(() => import("../shared/UeDetails"));
+
 const INIT_STATE = {
-  data: {
-    funktionsnedsattning: false,
-    funktionsnedsattningDesc: "",
-    utredningBehandling: false,
-    utredningBehandlingDesc: "",
-    arbetePaverkanSjukdomSkada: false,
-    arbetePaverkanSjukdomSkadaDesc: ""
-  }
+  metadata: {
+    certificateCode: "af00213",
+    certificateId: "bed26d3e-7112-4f08-98bf-01be40e26c80",
+    certificateName: "Arbetsförmedlingens medicinska utlåtande",
+  },
+  categories: {
+    funktionsnedsattningCategory: {
+      id: "funktionsnedsattning",
+      parent: null,
+      config: {
+        text: "Funktionsnedsättning",
+        description: null,
+        component: "category",
+      },
+    },
+    funktionsnedsattning: {
+      id: "1",
+      parent: "funktionsnedsattning",
+      config: {
+        text:
+          "Finns besvär på grund av sjukdom eller skada som medför funktionsnedsättning?",
+        description: "",
+        component: "ue-radio",
+      },
+      data: {
+        harFunktionsnedsattning: null,
+      },
+      validation: {
+        required: true,
+        requiredProp: "harFunktionsnedsattning",
+      },
+    },
+    harFunktionsnedsattning: {
+      id: "1.1",
+      parent: "1",
+      config: {
+        text:
+          "Beskriv de funktionsnedsättningar som har observerats (undersökningsfynd). Ange, om möjligt, varaktighet.",
+        description: "",
+        component: "ue-textarea",
+      },
+      data: {
+        funktionsnedsattning: null,
+      },
+      validation: {
+        required: true,
+        requiredProp: "funktionsnedsattning",
+        hideExpression: "!harFunktionsnedsattning",
+      },
+    },
+    aktivitetsbegransningCategory: {
+      id: "aktivitetsbegransning",
+      parent: null,
+      config: {
+        text: "Aktivitetsbegränsning",
+        description: null,
+        component: "category",
+      },
+      validation: {
+        hideExpression: "!model.harFunktionsnedsattning",
+      },
+    },
+    aktivitetsbegransning: {
+      id: "2",
+      parent: "aktivitetsbegransning",
+      config: {
+        text:
+          "Leder funktionsnedsättningarna till aktivitetsbegränsningar i relation till arbete eller studier?",
+        description: "",
+        component: "ue-radio",
+      },
+      data: {
+        harAktivitetsbegransning: null,
+      },
+      validation: {
+        required: true,
+        requiredProp: "harAktivitetsbegransning",
+      },
+    },
+    harAktivitetsbegransning: {
+      id: "2.1",
+      parent: "2",
+      config: {
+        text:
+          "Ange vilka aktivitetsbegränsningar? Ange hur och om möjligt varaktighet/prognos.",
+        description: "",
+        component: "ue-textarea",
+      },
+      data: {
+        aktivitetsbegransning: null,
+      },
+      validation: {
+        required: true,
+        requiredProp: "aktivitetsbegransning",
+        hideExpression: "!harAktivitetsbegransning",
+      },
+    },
+    utredningBehandlingCategory: {
+      id: "utredningBehandling",
+      parent: null,
+      config: {
+        text: "Utredning och behandling",
+        description: null,
+        component: "category",
+      },
+      validation: {
+        hideExpression: null,
+      },
+    },
+    utredningBehandling: {
+      id: "3",
+      parent: "utredningBehandling",
+      config: {
+        text:
+          "Finns pågående eller planerade utredningar/behandlingar som påverkar den planering som Arbetsförmedlingen har beskrivit i förfrågan?",
+        description: "",
+        component: "ue-radio",
+      },
+      data: {
+        harUtredningBehandling: null,
+      },
+      validation: {
+        required: true,
+        requiredProp: "harUtredningBehandling",
+      },
+    },
+    harUtredningBehandling: {
+      id: "3.1",
+      parent: "3",
+      config: {
+        text:
+          "Hur påverkar utredningarna/behandlingarna planeringen? När planeras utredningarna/behandlingarna att vara avslutade?\n",
+        description: "",
+        component: "ue-textarea",
+      },
+      data: {
+        utredningBehandling: null,
+      },
+      validation: {
+        required: true,
+        requiredProp: "utredningBehandling",
+        hideExpression: "!harUtredningBehandling",
+      },
+    },
+    arbetetsPaverkanCategory: {
+      id: "arbetetsPaverkan",
+      parent: null,
+      config: {
+        text: "Arbetets påverkan på sjukdom/skada",
+        description: null,
+        component: "category",
+      },
+      validation: {
+        hideExpression: null,
+      },
+    },
+    arbetetsPaverkan: {
+      id: "4",
+      parent: "arbetetsPaverkan",
+      config: {
+        text:
+          "Kan sjukdomen/skadan förvärras av vissa arbetsuppgifter/arbetsmoment?",
+        description: "",
+        component: "ue-radio",
+      },
+      data: {
+        harArbetetsPaverkan: null,
+      },
+      validation: {
+        required: true,
+        requiredProp: "harArbetetsPaverkan",
+      },
+    },
+    harArbetetsPaverkan: {
+      id: "4.1",
+      parent: "4",
+      config: {
+        text: "Vilken typ av arbetsuppgifter/arbetsmoment?",
+        description: "",
+        component: "ue-textarea",
+      },
+      data: {
+        arbetetsPaverkan: null,
+      },
+      validation: {
+        required: true,
+        requiredProp: "arbetetsPaverkan",
+        hideExpression: "!harArbetetsPaverkan",
+      },
+    },
+    ovrigtCategory: {
+      id: "ovrigt",
+      config: {
+        text: "Övrigt",
+        description: null,
+        component: "category",
+      },
+      validation: {
+        hideExpression: null,
+      },
+    },
+    ovrigt: {
+      id: "5",
+      parent: "ovrigt",
+      config: {
+        text: "Övrigt som Arbetsförmedlingen bör känna till?",
+        description: "",
+        component: "ue-textarea",
+      },
+      data: {
+        ovrigt: null,
+      },
+      validation: {
+        required: false,
+      },
+    },
+  },
 };
 function reducer(state, action) {
-  let { data } = state;
   switch (action.type) {
+    case "LOAD_DATA":
+      return { ...state, ...action.certificate };
     case "UPDATE":
-      data[action.name] = action.value;
-      return { ...state, data };
+      let categories = state.categories ? state.categories : {};
+      categories[action.prop].data[action.name] = action.value;
+
+      return { ...state, categories };
 
     default:
       throw new Error(`Unknown action type: ${action.type}`);
@@ -31,211 +249,219 @@ function reducer(state, action) {
 export default function Af00213() {
   const [state, dispatch] = useReducer(reducer, INIT_STATE);
 
-  const { data } = state;
+  const { categories, metadata } = state;
+
+  const webcertService = new WebcertMock();
+
+  useEffect(() => {
+    function loadCertficiateData() {
+      webcertService.getOneLevelData().then((response) => {
+        console.log("response:", response);
+        dispatch({
+          type: "LOAD_DATA",
+          categories: response.categories,
+          metadata: response.metadata,
+        });
+      });
+    }
+
+    loadCertficiateData();
+  }, []);
+
   return (
     <div>
       <Card className={styles.card}>
-        <Card.Header>Funktionsnedsättning</Card.Header>
+        <Card.Header>
+          {state?.categories?.funktionsnedsattningCategory?.config?.text}
+        </Card.Header>
         <Card.Text>
-          Finns besvär på grund av sjukdom eller skada som medför
-          funktionsnedsättning?
+          {state?.categories?.funktionsnedsattning?.config?.text}
         </Card.Text>
-        <Form.Check
-          value={data.funktionsnedsattning}
-          checked={data.funktionsnedsattning}
-          onChange={() =>
+
+        <UeRadio
+          name="funktionsnedsattning"
+          value={
+            state?.categories?.funktionsnedsattning.data.harFunktionsnedsattning
+          }
+          update={(value) =>
             dispatch({
               type: "UPDATE",
-              name: "funktionsnedsattning",
-              value: true
+              prop: "funktionsnedsattning",
+              name: "harFunktionsnedsattning",
+              value,
             })
           }
-          custom
-          inline
-          label="Ja"
-          type="radio"
-          id={`f1Ja`}
         />
-        <Form.Check
-          value={data.funktionsnedsattning}
-          checked={!data.funktionsnedsattning}
-          onChange={() =>
-            dispatch({
-              type: "UPDATE",
-              name: "funktionsnedsattning",
-              value: false
-            })
-          }
-          custom
-          inline
-          label="Nej"
-          type="radio"
-          id={`f1Nej`}
-        />
-        {data.funktionsnedsattning && (
-          <Form.Group controlId="funktionsnedsattningDesc">
-            <Form.Label required>
-              Beskriv de funktionsnedsättningar som har observerats
-              (undersökningsfynd). Ange, om möjligt, varaktighet
-            </Form.Label>
-            <Form.Control
-              as="textarea"
-              rows="5"
-              value={data.funktionsnedsattningDesc}
-              onChange={e =>
+
+        {state?.categories?.funktionsnedsattning.data
+          ?.harFunktionsnedsattning === "ja" && (
+          <Suspense fallback={<p>Laddar...</p>}>
+            <UeDetails
+              label={state?.categories?.harFunktionsnedsattning?.config?.text}
+              update={(value) =>
                 dispatch({
                   type: "UPDATE",
-                  name: "funktionsnedsattningDesc",
-                  value: e.target.value
+                  name: "harFunktionsnedsattning",
+                  prop: "funktionsnedsattning",
+                  value,
                 })
               }
             />
-          </Form.Group>
-        )}
-      </Card>
-      <Card className={styles.card}>
-        <Card.Header>Utredning och behandling</Card.Header>
-        <Card.Text>
-          Finns pågående eller planerade utredningar/behandlingar som påverkar
-          den planering som Arbetsförmedlingen har beskrivit i förfrågan?
-        </Card.Text>
-        <Form.Check
-          value={data.utredningBehandling}
-          checked={data.utredningBehandling}
-          onChange={() =>
-            dispatch({
-              type: "UPDATE",
-              name: "utredningBehandling",
-              value: true
-            })
-          }
-          custom
-          inline
-          label="Ja"
-          type="radio"
-          id={`f2Ja`}
-        />
-        <Form.Check
-          value={data.utredningBehandling}
-          checked={!data.utredningBehandling}
-          onChange={() =>
-            dispatch({
-              type: "UPDATE",
-              name: "utredningBehandling",
-              value: false
-            })
-          }
-          custom
-          inline
-          label="Nej"
-          type="radio"
-          id={`f2Nej`}
-        />
-        {data.utredningBehandling && (
-          <Form.Group controlId="utredningBehandlingDesc">
-            <Form.Label required>
-              Beskriv de funktionsnedsättningar som har observerats
-              (undersökningsfynd). Ange, om möjligt, varaktighet
-            </Form.Label>
-            <Form.Control
-              as="textarea"
-              rows="5"
-              value={data.utredningBehandlingDesc}
-              onChange={e =>
-                dispatch({
-                  type: "UPDATE",
-                  name: "utredningBehandlingDesc",
-                  value: e.target.value
-                })
-              }
-            />
-          </Form.Group>
+          </Suspense>
         )}
       </Card>
 
+      {/* aktivitetsbegransningCategory */}
+      {state?.categories?.funktionsnedsattning.data.harFunktionsnedsattning ===
+        "ja" && (
+        <Card className={styles.card}>
+          <Card.Header>
+            {state?.categories?.aktivitetsbegransningCategory?.config?.text}
+          </Card.Header>
+          <Card.Text>
+            {state?.categories?.aktivitetsbegransning?.config?.text}
+          </Card.Text>
+
+          <UeRadio
+            name="aktivitetsbegransning"
+            value={
+              state?.categories?.aktivitetsbegransning.data
+                ?.harAktivitetsbegransning
+            }
+            update={(value) =>
+              dispatch({
+                type: "UPDATE",
+                prop: "aktivitetsbegransning",
+                name: "harAktivitetsbegransning",
+                value,
+              })
+            }
+          />
+
+          {state?.categories?.aktivitetsbegransning.data
+            ?.harAktivitetsbegransning === "ja" && (
+            <Suspense fallback={<p>Laddar...</p>}>
+              <UeDetails
+                label={
+                  state?.categories?.harAktivitetsbegransning?.config?.text
+                }
+                update={(value) =>
+                  dispatch({
+                    type: "UPDATE",
+                    name: "harAktivitetsbegransning",
+                    prop: "aktivitetsbegransning",
+                    value,
+                  })
+                }
+              />
+            </Suspense>
+          )}
+        </Card>
+      )}
+
       <Card className={styles.card}>
-        <Card.Header>Arbetets påverkan på sjukdom/skada</Card.Header>
+        <Card.Header>
+          {state?.categories?.utredningBehandlingCategory.config.text}
+        </Card.Header>
         <Card.Text>
-          Kan sjukdomen/skadan förvärras av vissa arbetsuppgifter/arbetsmoment?
+          {state?.categories?.utredningBehandling.config.text}
         </Card.Text>
-        <Form.Check
-          value={data.arbetePaverkanSjukdomSkada}
-          checked={data.arbetePaverkanSjukdomSkada}
-          onChange={() =>
+        <UeRadio
+          name="harUtredningBehandling"
+          value={
+            state?.categories?.utredningBehandling.data.harUtredningBehandling
+          }
+          update={(value) =>
             dispatch({
               type: "UPDATE",
-              name: "arbetePaverkanSjukdomSkada",
-              value: true
+              prop: "utredningBehandling",
+              name: "harUtredningBehandling",
+              value,
             })
           }
-          custom
-          inline
-          label="Ja"
-          type="radio"
-          id={`f3Ja`}
         />
-        <Form.Check
-          value={data.arbetePaverkanSjukdomSkada}
-          checked={!data.arbetePaverkanSjukdomSkada}
-          onChange={() =>
-            dispatch({
-              type: "UPDATE",
-              name: "arbetePaverkanSjukdomSkada",
-              value: false
-            })
-          }
-          custom
-          inline
-          label="Nej"
-          type="radio"
-          id={`f3Nej`}
-        />
-        {data.arbetePaverkanSjukdomSkada && (
-          <Form.Group controlId="arbetePaverkanSjukdomSkadaDesc">
-            <Form.Label required>
-              Beskriv de funktionsnedsättningar som har observerats
-              (undersökningsfynd). Ange, om möjligt, varaktighet
-            </Form.Label>
-            <Form.Control
-              as="textarea"
-              rows="5"
-              value={data.arbetePaverkanSjukdomSkadaDesc}
-              onChange={e =>
+
+        {state?.categories?.utredningBehandling.data.harUtredningBehandling ===
+          "ja" && (
+          <Suspense fallback={<p>Laddar...</p>}>
+            <UeDetails
+              label={state?.categories?.harUtredningBehandling?.config?.text}
+              value={
+                state?.categories?.harUtredningBehandling?.data
+                  ?.utredningBehandling
+              }
+              update={(value) =>
                 dispatch({
                   type: "UPDATE",
-                  name: "arbetePaverkanSjukdomSkadaDesc",
-                  value: e.target.value
+                  name: "harUtredningBehandling",
+                  prop: "utredningBehandling",
+                  value,
                 })
               }
             />
-          </Form.Group>
+          </Suspense>
         )}
       </Card>
 
       <Card className={styles.card}>
-        <Card.Header>Vårdenhetens adress</Card.Header>
+        <Card.Header>
+          {state?.categories?.arbetetsPaverkanCategory?.config.text}
+        </Card.Header>
+        <Card.Text>
+          {state?.categories?.arbetetsPaverkan?.config.text}
+        </Card.Text>
 
-        <label htmlFor="Postadress">Postadress</label>
-        <InputGroup className="mb-3">
-          <FormControl id="Postadress" aria-describedby="Postadress" />
-        </InputGroup>
+        <UeRadio
+          name="hararbetsPaverkan"
+          value={state.categories.arbetetsPaverkan.data.harArbetetsPaverkan}
+          update={(value) =>
+            dispatch({
+              type: "UPDATE",
+              prop: "arbetetsPaverkan",
+              name: "harArbetetsPaverkan",
+              value,
+            })
+          }
+        />
 
-        <label htmlFor="Postnummer">Postnummer</label>
-        <InputGroup className="mb-3">
-          <FormControl id="Postnummer" aria-describedby="Postnummer" />
-        </InputGroup>
-
-        <label htmlFor="Postort">Postort</label>
-        <InputGroup className="mb-3">
-          <FormControl id="Postort" aria-describedby="Postort" />
-        </InputGroup>
-
-        <label htmlFor="Telefonnummer">Telefonnummer</label>
-        <InputGroup className="mb-3">
-          <FormControl id="Telefonnummer" aria-describedby="Telefonnummer" />
-        </InputGroup>
+        {state.categories.arbetetsPaverkan.data.harArbetetsPaverkan ===
+          "ja" && (
+          <UeDetails
+            label="Vilken typ av arbetsuppgifter/arbetsmoment?"
+            value={
+              state?.categories?.harArbetetsPaverkan?.data?.arbetetsPaverkan
+            }
+            update={(value) =>
+              dispatch({
+                type: "UPDATE",
+                prop: "harArbetetsPaverkan",
+                name: "arbetetsPaverkan",
+                value,
+              })
+            }
+          />
+        )}
       </Card>
+      <Card className={styles.card}>
+        <Card.Header>
+          {state?.categories.ovrigtCategory.config.text}
+        </Card.Header>
 
+        <Suspense fallback={<p>Laddar...</p>}>
+          <UeDetails
+            label={state.categories.ovrigt.config.text}
+            value={state.categories.ovrigt.data.ovrigt}
+            update={(value) =>
+              dispatch({
+                type: "UPDATE",
+                prop: "ovrigt",
+                name: "ovrigt",
+                value,
+              })
+            }
+          />
+        </Suspense>
+      </Card>
       <Button>Signera och skicka</Button>
     </div>
   );
